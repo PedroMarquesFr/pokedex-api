d728c21819f00ee1b60386ecd49ac864db7b1945
Pedro Marques Freire <pedromarques1011@gmail.com>
2022-09-12 23:38:51 -0300

    feat: rea 2, 3, 5

--- src/CustomerCrud/Controllers/CustomerController.cs
+++ src/CustomerCrud/Controllers/CustomerController.cs
@@ -22,4 +22,42 @@ public class CustomerController : ControllerBase
         if (customers == null) return NotFound("Customers not found");
         return Ok(customers);
     }
+
+    [HttpGet("{id}", Name = "GetById")]
+    public ActionResult GetById(int id)
+    {
+        var customer = CustomerRepository.GetById(id);
+        if (customer == null) return NotFound("Customer not found");
+        return Ok(customer);
+    }
+
+    [HttpPost(Name = "CreateCustomer")]
+    public ActionResult CreateCustomer([FromBody] CustomerRequest request)
+    {
+        int id = CustomerRepository.GetNextIdValue();
+        var customer = new Customer(id, request);
+        var isCreated = CustomerRepository.Create(customer);
+
+        string actionName = nameof(GetById);
+        // var controllerName = "CustomerController";
+        var routeValues = new { id, version = "1.0" };
+
+        return CreatedAtAction(actionName, new { id = customer.Id }, customer);
+    }
+
+    [HttpPut("{id}", Name = "UpdateCustomer")]
+    public ActionResult UpdateCustomer(int id, [FromBody] CustomerRequest request)
+    {
+        bool customerUpdated = CustomerRepository.Update(id, request);
+        if (!customerUpdated) return NotFound("Customer not found");
+        return Ok($"Customer {id} updated");
+    }
+
+    [HttpDelete("{id}", Name = "DeleteCustomer")]
+    public ActionResult Delete(int id)
+    {
+        bool result = CustomerRepository.Delete(id);
+        if (!result) return NotFound("Customer not found");
+        return NoContent();
+    }
 }
