58ff2a26dfad31bf05854f661b5ad07e8cd1c87d
Pedro Marques Freire <pedromarques1011@gmail.com>
2022-09-12 23:39:01 -0300

    feat: rea 2, 3, 5

--- src/CustomerCrud.Test/CustomerControllerTest.cs
+++ src/CustomerCrud.Test/CustomerControllerTest.cs
@@ -40,24 +40,62 @@ public class CustomersControllerTest : IClassFixture<WebApplicationFactory<Progr
     [Fact]
     public async Task GetByIdTest()
     {
-        throw new NotImplementedException();
+        var customer = AutoFaker.Generate<Customer>();
+        _repositoryMock.Setup(st => st.GetById(0)).Returns(customer);
+
+        var response = await _client.GetAsync("/controller/0");
+        var content = await response.Content.ReadFromJsonAsync<Customer>();
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        content.Should().BeEquivalentTo(customer);
+
+        _repositoryMock.Verify(db => db.GetById(0), Times.Once);
     }
 
     [Fact]
     public async Task CreateTest()
     {
-        throw new NotImplementedException();
+        var request = AutoFaker.Generate<CustomerRequest>();
+        _repositoryMock.Setup(st => st.GetNextIdValue()).Returns(1);
+        _repositoryMock.Setup(st => st.Create(It.Is<Customer>(r => r.Id == 1))).Returns(true);
+
+        var response = await _client.PostAsJsonAsync("/controller", request);
+        var content = await response.Content.ReadFromJsonAsync<Customer>();
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        content.Id.Should().Be(1);
+        content.Name.Should().Be(request.Name);
+        content.CPF.Should().Be(request.CPF);
+        content.Transactions.Should().BeEquivalentTo(request.Transactions);
+
+        _repositoryMock.Verify(db => db.GetNextIdValue(), Times.Once);
+        _repositoryMock.Verify(db => db.Create(It.Is<Customer>(r => r.Id == 1)), Times.Once);
     }
 
     [Fact]
     public async Task UpdateTest()
     {
-        throw new NotImplementedException();
+        var request = AutoFaker.Generate<CustomerRequest>();
+        _repositoryMock.Setup(st => st.Update(1, request)).Returns(true);
+
+        var response = await _client.PutAsJsonAsync("/controller/1", request);
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        content.Should().Be("Customer 1 updated");
+        _repositoryMock.Verify(db => db.Update(0, request), Times.Once);
     }
 
     [Fact]
     public async Task DeleteTest()
     {
-        throw new NotImplementedException();
+        _repositoryMock.Setup(st => st.Delete(1)).Returns(true);
+
+        var response = await _client.DeleteAsync("/controller/1");
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        _repositoryMock.Verify(db => db.Delete(1), Times.Once);
     }
 }
